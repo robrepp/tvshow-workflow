5ea28d913ebf69c71a43470527dc9bda595f84b8
Justin Reid <justin@justinreid.ca>
2010-10-06 16:10:05 -0700

    Adding config file instead of having options inside the script.

--- tvshow-workflow.pl
+++ tvshow-workflow.pl
@@ -33,10 +33,21 @@ if ( $show ne "" ){
 return 0;
 }
 
-# config
-my $AP_bin="/Applications/AtomicParsley";
-my $HB_CLI_bin="/Applications/HandBrakeCLI";
-my $iTunes_auto_import_dir="/Volumes/1TB/iTunes/Automatically Add to iTunes";
+# load config file and set variables
+open(CONFIG,"tvshow-workflow.config") or die $_;
+my %config = ();
+while (<CONFIG>) {
+    chomp;
+    next if /^\s*\#/;
+    next unless /=/;
+    my ($key, $variable) = split(/\s*=\s*/,$_,2);
+    $variable =~ s/(\$(\w+))/$config{$2}/g;
+    $config{$key} = $variable;
+}
+
+my $AP_bin=$config{'AtomicParsleyLocation'};
+my $HB_CLI_bin=$config{'HandBrakeCLILocation'};
+my $iTunes_auto_import_dir=$config{'iTunesAutoImportLocation'};
 
 # file extensions to scan for
 my $include="\'.avi|.mkv\'";
@@ -65,15 +76,17 @@ unless (-d "./Staging/Encoding") {
 # list video files and assign that list to the videolist array
 my @videolist = `ls -1 | grep -Ei $include`;
 
-# main loop
+# moving all files to originals folder
 foreach my $videofile (@videolist){
-	
 	# eat the return character at the end of the file name
 	chomp $videofile;
 	
 	# move original file to Originals folder
 	`mv "$videofile" ./Staging/Originals/"$videofile"`;
-	
+}
+
+# main loop
+foreach my $videofile (@videolist){
 	# retrieve show name and replace periods with spaces
 	my $newShowName = $videofile;
 	$newShowName =~ s/(^.*)(\.s\d+e\d+.*)/$1/i;
@@ -117,32 +130,27 @@ foreach my $videofile (@videolist){
 		
 		# encode file with HandBrakeCLI
 		print "\nEncoding file... (Start time: ". POSIX::strftime('%H:%M:%S', localtime).")";
-		my $HBrun = `$HB_CLI_bin -i "./Staging/Originals/$videofile" -o "./Staging/Encoding/$newFileName" --preset="High Profile" > /dev/null 2>&1`;
+		my $HBrun = `$HB_CLI_bin -i "./Staging/Originals/$videofile" -o "./Staging/Encoding/$newFileName" --preset="AppleTV" > /dev/null 2>&1`;
 		print "\nEncoding complete. (End time: ". POSIX::strftime('%H:%M:%S', localtime).")\n##########\n";
 		
 		# use AtomicParsley to write the data to the file
 		my $APrun = `"$AP_bin" "./Staging/Encoding/$newFileName" --TVShowName "$show_info[0]" --artist "$show_info[0]" --TVEpisode "$newEpisode" --title "$show_info[5]" --TVEpisodeNum "$newEpisode" --tracknum "$newEpisode" --TVSeasonNum "$newSeason" --album "Season $newSeason" --TVNetwork "$show_info[11]" --genre "$show_info[10]" --stik "TV Show" -o "./Staging/Tagged/$newFileName"`;
 		
-		# establish final path to tagged file for Applescript
+		# establish final path to tagged file
 		my $finalPath = `pwd`;
 		chomp $finalPath;
 		$finalPath .= "/Staging/Tagged/$newFileName";
 		
 		# check if file exists before proceeding with import, move, and delete
 		if (-e $finalPath) {
-		
-		# import into iTunes
-		#`osascript -e 'tell application "iTunes" to activate'`;
-		#`osascript -e 'tell application "iTunes" to add (POSIX file "$finalPath")'`;
-		
-		# copy file to Auto-import iTunes directory
-		`cp ./Staging/Tagged/"$newFileName" "$iTunes_auto_import_dir"`;
-		
-		# move new file to Imported folder
-		`mv ./Staging/Tagged/"$newFileName" ./Staging/Imported/"$newFileName"`;
-		
-		# delete file in Encoding directory
-		unlink("./Staging/Encoding/$newFileName");
+			# copy file to Auto-import iTunes directory
+			`cp ./Staging/Tagged/"$newFileName" "$iTunes_auto_import_dir"`;
+			
+			# move new file to Imported folder
+			`mv ./Staging/Tagged/"$newFileName" ./Staging/Imported/"$newFileName"`;
+			
+			# delete file in Encoding directory
+			unlink("./Staging/Encoding/$newFileName");
 		}
 		
 		else {
